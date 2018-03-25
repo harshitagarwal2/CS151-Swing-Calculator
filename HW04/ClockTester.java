48dbc93eae0681bcbee65579f367c4da250194cd
liangtonyy <34144422+liangtonyy@users.noreply.github.com>
2018-03-24 22:05:42 -0700

    Update ClockTester.java

--- HW04/ClockTester.java
+++ HW04/ClockTester.java
@@ -3,39 +3,41 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class ClockTester {
-	public static void main(String args[]) {
-		JFrame frame = new JFrame();
+   
+    public static void main(String[] args) {
+       JFrame frame = new JFrame();
+       
+       // set layout manager
+       frame.setLayout(new BorderLayout());
 
-		// set layout manager
-		frame.setLayout(new BorderLayout());
+       JPanel topNav = new JPanel(new FlowLayout());
+       topNav.add(clockButton);
+       topNav.add(stopwatchButton);
+       frame.add(topNav, BorderLayout.NORTH);
+       
+       MyClock clock = new MyClock(0, 0, CLOCK_RADIUS );
+       
+     
+       JPanel clockPanel = new JPanel();
+       clockPanel.setLayout(new OverlayLayout(clockPanel));
+       clockPanel.add(clock);
+      
+       frame.add(clockPanel, BorderLayout.CENTER);
 
-		// create swing components
-		ClockFace icon = new ClockFace(0, 0, CLOCK_RADIUS);
-		JPanel topNav = new JPanel(new FlowLayout());
-		ClockHand secondsHand = new ClockHand();
 
-		// add swing components to content pane
-		icon.add(secondsHand);
-		
-		topNav.add(new JButton("clock"));
-		topNav.add(new JButton("stopwatch"));
-		
-		frame.add(icon, BorderLayout.CENTER);
-		frame.add(topNav, BorderLayout.NORTH);
 
-		icon.repaint();
-
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.pack();
-		frame.setVisible(true);
-
-		// add behavior- like pressing "stopwatch" and "clock" buttons to display their respective components
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       frame.pack();
+       frame.setVisible(true);
+       
+   	// add behavior- like pressing "stopwatch" and "clock" buttons to display their respective components
 		// button.addActionListener(new ActionListener() {
 		//		public void actionPerformed(ActionEvent e) {
 		//
 		//		}
 		// })
-	}
-
-	private static final int CLOCK_RADIUS = 500;
+   }
+    private static final int CLOCK_RADIUS = 500;
+    private static JButton clockButton = new JButton("Clock");
+    private static JButton stopwatchButton = new JButton("Stopwatch");
 }
