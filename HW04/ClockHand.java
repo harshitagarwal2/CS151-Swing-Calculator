c7c35d4b422419693ec8e2e87cfdd2aa278c95b6
liangtonyy <34144422+liangtonyy@users.noreply.github.com>
2018-03-24 22:05:14 -0700

    Update ClockHand.java

--- HW04/ClockHand.java
+++ HW04/ClockHand.java
@@ -1,74 +1,64 @@
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.Graphics2D;
+import java.awt.*;
 import java.awt.geom.*;
-import java.util.Calendar;
-import java.util.TimeZone;
-
-import javax.swing.Timer;
+import java.util.*;
 
 public class ClockHand implements MoveableShape {
-	private int x;
-	private int y;
-	private int width;
-	private int height;
-	private String color;
-	private String handType;
+	private double x;
+	private double y;
+	private double xaxis;
+	private double yaxis;
+	private double startingTick;
+	private Color color;
 
-	public ClockHand(int x, int y, int width, int height, String handType) {
+
+    public ClockHand(double x, double y , double xaxis, double yaxis, double startingTick,Color color) {
+    	//this.color = color;
+    	this.x = x;
+		this.y =y;
+		this.xaxis =xaxis ;
+		this.yaxis = yaxis;	
+		this.startingTick = startingTick;
 		this.color = color;
-		this.width = width;
-		this.x = x;
-		this.y = y;
-		this.height = height;
-		this.handType = handType;
-	}
+    }
+    
+    public void move() {
+        startingTick++;  
+        if (startingTick == 60){
+           startingTick = 0;
+        }
+    }
+    
+    @Override
+   	public void translate(double dx, double dy) {
+   		xaxis= dx;
+   		yaxis =dy;
+   		}
+    
+    public void reset() {
+        startingTick = 0;
+    }
+	
 
-	@Override
-	public void draw(Graphics2D g2) {
-		int r = width * 2;
-		int adjustedY = x + (r - 15);
-		
-		Rectangle2D.Double hand = new Rectangle2D.Double(x, y, width, height);
+    @Override
+    public void draw(Graphics2D g2) {
+        GeneralPath path = new GeneralPath();
+        path.moveTo(x, y);
+     
+        double angleFrom12 = 2*Math.PI*(startingTick/60.0);
+        double deltaX = Math.cos(Math.PI*1.5+angleFrom12)*xaxis;
+        double deltaY = (Math.sin(Math.PI*1.5+angleFrom12)*xaxis);
+      
+        path.lineTo(x + deltaX, y + deltaY);
+        path.lineTo(x + deltaX, y + deltaY);
 
-		Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
-
-		if (handType == "SECOND") {
-			g2.setPaint(Color.RED);
-			
-			double second = (double) (calendar.get(Calendar.SECOND));
-			double secondAngle = second / 60.0 * 2.0 * Math.PI;
-
-			g2.rotate((float) secondAngle, (int) x, (int) adjustedY);
-		}
-		
-		if (handType == "MINUTE") {
-			g2.setPaint(Color.BLACK);
-			
-			double minute = (double)(calendar.get(Calendar.MINUTE)) +
-			        (double)(calendar.get(Calendar.SECOND))/60.0;
-
-			double minuteAngle = minute/60.0*2.0*Math.PI;
-			
-			g2.rotate((float)minuteAngle, (int)x, (int)adjustedY);
-		}
-		
-		if (handType == "HOUR") {
-			g2.setPaint(Color.BLACK);
-			
-			double hour = (double)(calendar.get(Calendar.HOUR_OF_DAY)%12) +
-			        (double)(calendar.get(Calendar.MINUTE))/60.0;
-
-			double angle = hour/12.0*2.0*Math.PI;
-			g2.rotate((float)angle, (int)x, (int)adjustedY);
-		}
-
-		g2.fill(hand);
-	}
-
-	@Override
-	public void translate(int dx, int dy) {
-	}
-
+        g2.setStroke(new BasicStroke((float)yaxis));
+        g2.setColor(color);
+        g2.draw(path);
+    }
+    
+    public double getstartingTick(){
+       return startingTick;
+    }
+    
+   
 }
-
