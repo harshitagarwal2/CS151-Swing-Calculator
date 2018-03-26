abc49d64c400e728364540cf3e8ecd0977be7bbb
liangtonyy <34144422+liangtonyy@users.noreply.github.com>
2018-03-25 19:23:29 -0700

    Update ClockTester.java

--- HW04/ClockTester.java
+++ HW04/ClockTester.java
@@ -1,11 +1,16 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.util.*;
 
 public class ClockTester {
    
     public static void main(String[] args) {
-       JFrame frame = new JFrame();
+    	
+    	Stopwatch stopwatch = new Stopwatch(0, 0, CLOCK_RADIUS);
+        MyClock clock = new MyClock(0, 0, CLOCK_RADIUS );
+    	
+    	JFrame frame = new JFrame();
        
        // set layout manager
        frame.setLayout(new BorderLayout());
@@ -15,27 +20,29 @@ public class ClockTester {
        topNav.add(stopwatchButton);
        frame.add(topNav, BorderLayout.NORTH);
        
-       MyClock clock = new MyClock(0, 0, CLOCK_RADIUS );
-       
-     
        JPanel clockPanel = new JPanel();
        clockPanel.setLayout(new OverlayLayout(clockPanel));
        clockPanel.add(clock);
-      
+       clockPanel.add(stopwatch);
        frame.add(clockPanel, BorderLayout.CENTER);
-
-
+       
+      
+       stopwatch.setVisible(false);
+       
+       clockButton.addActionListener(event -> {
+           stopwatch.setVisible(false);
+           clock.setVisible(true);
+       });
+       stopwatchButton.addActionListener(event -> {
+           stopwatch.reset();
+           clock.setVisible(false);
+           stopwatch.setVisible(true);
+       });
 
        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        frame.pack();
        frame.setVisible(true);
-       
-   	// add behavior- like pressing "stopwatch" and "clock" buttons to display their respective components
-		// button.addActionListener(new ActionListener() {
-		//		public void actionPerformed(ActionEvent e) {
-		//
-		//		}
-		// })
+    
    }
     private static final int CLOCK_RADIUS = 500;
     private static JButton clockButton = new JButton("Clock");
